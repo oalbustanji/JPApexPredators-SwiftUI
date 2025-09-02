694549d7b825dc5a2f604ad3a17557725c34a7fa
omaralbutanji <160024861+omaralbutanji@users.noreply.github.com>
2025-09-02 22:18:29 +0300

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Omar Bustanji on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
